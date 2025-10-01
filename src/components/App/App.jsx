c9e524bdac6fd00bd62b8f537c79b6ad156f06a0
bndnt <67003594+bndnt@users.noreply.github.com>
2025-10-01 13:55:33 -0400

    lazy+suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,24 @@
 import css from "./App.module.css";
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Loader from "../Loader/Loader";
 
-import Navigation from "../../components/Navigation/Navigation";
-import HomePage from "../../pages/HomePage";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
-import MovieVideo from "../MovieVideo/MovieVideo";
-
+// import Navigation from "../../components/Navigation/Navigation";
+// import HomePage from "../../pages/HomePage";
+// import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+// import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
+// import MovieCast from "../MovieCast/MovieCast";
+// import MovieReviews from "../MovieReviews/MovieReviews";
+// import MovieVideo from "../MovieVideo/MovieVideo";
+const Navigation = lazy(() => import("../../components/Navigation/Navigation"));
+const HomePage = lazy(() => import("../../pages/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+const MovieVideo = lazy(() => import("../MovieVideo/MovieVideo"));
 function App() {
   return (
     <div>
@@ -21,16 +30,18 @@ function App() {
       <main className={css.main}>
         <div className="mainWrap">
           <div className={css.container}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/movies" element={<MoviesPage />} />
-              <Route path="//movies/:movieId" element={<MovieDetailsPage />}>
-                <Route index element={<Navigate to="video" replace />} />
-                <Route path="video" element={<MovieVideo />} />
-                <Route path="cast" element={<MovieCast />} />
-                <Route path="reviews" element={<MovieReviews />} />
-              </Route>
-            </Routes>
+            <Suspense fallback={<Loader />}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/movies" element={<MoviesPage />} />
+                <Route path="//movies/:movieId" element={<MovieDetailsPage />}>
+                  <Route index element={<Navigate to="video" replace />} />
+                  <Route path="video" element={<MovieVideo />} />
+                  <Route path="cast" element={<MovieCast />} />
+                  <Route path="reviews" element={<MovieReviews />} />
+                </Route>
+              </Routes>
+            </Suspense>
           </div>
         </div>
       </main>
